04dbad931a37517b823dd011bc5551ecd5e10bd4
DimitrisMilonopoulos <dimitriosmilonopoulos@gmail.com>
2019-02-28 20:50:24 +0200

    Ma boi

--- classes.py
+++ classes.py
@@ -1,17 +1,19 @@
 class picture:
-    def  __init__(self, ID , tags , Orientation , Notags ):
-        self.ID = []
+    def __init__(self, ID, tags, Orientation, Notags):
+        self.ID = ID
         self.tags = tags
         self.orientation = Orientation
-        self.tagsNo=Notags
+        self.tagsNo = Notags
+
+
 class slide:
-    def __init__(self ,Pictures,id ):
-        self.tags=set()
-        if(len(Pictures)==2):
-            self.type="V"
+    def __init__(self, Pictures, id):
+        self.tags = set()
+        if(len(Pictures) == 2):
+            self.type = "V"
         else:
-            self.type="H"
+            self.type = "H"
         for Picture in Pictures:
             for tag in Picture:
                 self.tags.add(tag)
-        self.id=id        
+        self.id = id
