2e6afa40f0fb407bffc4db69dcb6eed63d62a821
DimitrisMilonopoulos <dimitriosmilonopoulos@gmail.com>
2019-02-28 20:44:12 +0200

    New Class

--- Read.py
+++ Read.py
@@ -12,10 +12,11 @@ def ReadInput(filename):
     for j, line in enumerate(lines):
         temp_list = set()
         orientation = line[0]
+        length = int(line[1])
         for i in range(2, len(line)):
             temp_list.add(line[i])
 
-        photo_list.append(classes.picture(j, temp_list, orientation))
+        photo_list.append(classes.picture(j, temp_list, orientation, length))
 
     return (N, photo_list)
 
