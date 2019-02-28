9287d38d3c4c232c7db66117ae49ba08b50ef296
klaeug farseason <EUKLA>
2019-02-28 19:59:56 +0200

    Pic

---
+++ classes.py
@@ -0,0 +1,13 @@
+class picture:
+    def  __init__(self, ID , tags , orientation):
+        self.ID = []
+        self.tags = tags
+        self.orientation = Orientation
+class slide:
+    def __init__(self ,Picture):
+        if(len(Picture)==2):
+            self.type="V"
+        else:
+            self.type="H"
+        
+        
