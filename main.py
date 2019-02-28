2300bc2d80f93390727179e7f8de8482c9e924a3
DimitrisMilonopoulos <dimitriosmilonopoulos@gmail.com>
2019-02-28 21:50:56 +0200

    Fix main

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import Read
 
 for i in range(5):
     if i == 0:
-        N, photo_list = Read.ReadInput("a_example.txt")
+        N, photo_list, vertical_list = Read.ReadInput("a_example.txt")
     elif i == 1:
-        N, photo_list = Read.ReadInput("b_lovely_landscapes.txt")
+        N, photo_list, vertical_list = Read.ReadInput(
+            "b_lovely_landscapes.txt")
     elif i == 2:
-        N, photo_list = Read.ReadInput("c_memorable_moments.txt")
+        N, photo_list, vertical_list = Read.ReadInput(
+            "c_memorable_moments.txt")
     elif i == 3:
-        N, photo_list = Read.ReadInput("d_pet_pictures.txt")
+        N, photo_list, vertical_list = Read.ReadInput("d_pet_pictures.txt")
     elif i == 4:
-        N, photo_list = Read.ReadInput("e_shiny_selfies.txt")
+        N, photo_list, vertical_list = Read.ReadInput("e_shiny_selfies.txt")
 
     tagDict = functions.getTagDict(photo_list)
 
