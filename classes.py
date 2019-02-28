e587767b9f34a5313b4767ba092557e7c9767cc4
klaeug farseason <EUKLA>
2019-02-28 20:13:37 +0200

    Slides

--- classes.py
+++ classes.py
@@ -1,17 +1,17 @@
 class picture:
-    def  __init__(self, ID , tags , Orientation):
+    def  __init__(self, ID , tags , Orientation , Notags ):
         self.ID = []
         self.tags = tags
         self.orientation = Orientation
-        self.tagsNo=len(tags)
+        self.tagsNo=Notags
 class slide:
     def __init__(self ,Pictures,id ):
-        self.tags=[]
+        self.tags=set()
         if(len(Pictures)==2):
             self.type="V"
         else:
             self.type="H"
         for Picture in Pictures:
             for tag in Picture:
-                self.tags.append(tag)
+                self.tags.add(tag)
         self.id=id        
