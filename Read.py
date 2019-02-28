e408273540e3285600875982ca9b0bdae0c109a7
DimitrisMilonopoulos <dimitriosmilonopoulos@gmail.com>
2019-02-28 21:53:52 +0200

    Fix

--- Read.py
+++ Read.py
@@ -6,20 +6,23 @@ def ReadInput(filename):
     lines = [line.rstrip('\n').split(' ') for line in FileDesc]
 
     photo_list = []
-    verticalphoto=[]
+    verticalphoto = []
     N = int(lines[0][0])
     del lines[0]
     for j, line in enumerate(lines):
         temp_list = set()
         orientation = line[0]
         length = int(line[1])
-        if(orientation=="V"):
+        if(orientation == "V"):
             verticalphoto.append(j)
         for i in range(2, len(line)):
             temp_list.add(line[i])
-        photo_list.append(classes.picture(j, temp_list, orientation, length))
+            x = classes.picture(j, temp_list, orientation, length)
+        photo_list.append(x)
+        if(orientation == "V"):
+            verticalphoto.append(x)
 
-    return (N, photo_list , verticalphoto)
+    return (N, photo_list, verticalphoto)
 
 
 #print ReadInput("a_example.txt")
