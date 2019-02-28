19eac1727c73b70f599d85104c46c1974bb6b216
klaeug farseason <EUKLA>
2019-02-28 22:42:26 +0200

    JIJEIFEF]

--- functions.py
+++ functions.py
@@ -56,14 +56,14 @@ def removePhoto(tagDict, vlist, photo):
     return (tagDict,vlist)
 
  def FindinngVH(photo1,photo2,verticalphoto):
-     mymax=0
-     myset=set()
-     for photo in verticalphoto:
-         myset.join(photo1.tags)
-         myset.join(photo.tags)
-         sample=getScore(myset,photo2)
-         if(sample>mymax):
-             mymax=sample
-             chosenphoto=photo
+        mymax=0
+        myset=set()
+        for photo in verticalphoto:
+            myset.join(photo1.tags)
+            myset.join(photo.tags)
+            sample=getScore(myset,photo2)
+            if(sample>mymax):
+                mymax=sample
+                chosenphoto=photo
         myset=set()
     return chosenphoto
