1a38b1c9a6c1d780609ce2a8671e949e1c59d462
DimitrisMilonopoulos <dimitriosmilonopoulos@gmail.com>
2019-02-28 21:25:04 +0200

    New shit

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 import Read
+import classes
+
 
 def getTagDict(Photos):
     tagDict = {}
@@ -7,8 +9,22 @@ def getTagDict(Photos):
             if tag in tagDict:
                 x = tagDict[tag]
                 x.append(photo)
-                tagDict[tag]= x
+                tagDict[tag] = x
                 #tagDict[tag] = tagDict[tag].append(photo)
             else:
-                tagDict[tag]=[photo]
+                tagDict[tag] = [photo]
     return tagDict
+
+# calculate the score of BOTH pictures and slides
+
+
+def getScore(Slide1, Slide2):
+    list = []
+    x = Slide1.tags.intersection(Slide2.tags)
+    list.append(len(x))
+    x = Slide1.tags.difference(Slide2.tags)
+    list.append(len(x))
+    x = Slide2.tags.difference(Slide1.tags)
+    list.append(len(x))
+
+    return min(list)
