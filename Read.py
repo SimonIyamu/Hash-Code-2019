45296899095d3c67e1189e9e9cf23a1009328264
DimitrisMilonopoulos <dimitriosmilonopoulos@gmail.com>
2019-02-28 20:10:13 +0200

    Read vol 2

--- Read.py
+++ Read.py
@@ -10,10 +10,10 @@ def ReadInput(filename):
     N = int(lines[0][0])
     del lines[0]
     for j, line in enumerate(lines):
-        temp_list = []
+        temp_list = set()
         orientation = line[0]
         for i in range(2, len(line)):
-            temp_list.append(line[i])
+            temp_list.add(line[i])
 
         photo_list.append(classes.picture(j, temp_list, orientation))
 
