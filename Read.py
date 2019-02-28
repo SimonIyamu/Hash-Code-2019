ebd0a49a5a8c0d48737b11bd38089b706b6c82b6
klaeug farseason <EUKLA>
2019-02-28 21:34:43 +0200

    VerticalPhoto

--- Read.py
+++ Read.py
@@ -6,19 +6,20 @@ def ReadInput(filename):
     lines = [line.rstrip('\n').split(' ') for line in FileDesc]
 
     photo_list = []
-
+    verticalphoto=[]
     N = int(lines[0][0])
     del lines[0]
     for j, line in enumerate(lines):
         temp_list = set()
         orientation = line[0]
         length = int(line[1])
+        if(orientation=="V"):
+            verticalphoto.append(j)
         for i in range(2, len(line)):
             temp_list.add(line[i])
-
         photo_list.append(classes.picture(j, temp_list, orientation, length))
 
-    return (N, photo_list)
+    return (N, photo_list , verticalphoto)
 
 
 #print ReadInput("a_example.txt")
